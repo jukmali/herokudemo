cfdbf5f114e9412bfb80406bc44b34e61e145ecc
jukmali <54353202+jukmali@users.noreply.github.com>
2022-01-26 19:48:26 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const PORT = progress.env.PORT || 3000
+var http = require("http");
+
+// Create server object
+http
+    .createServer(function(request,response){
+        response.writeHead(200,{"Content-Type":"text/plain"});
+        response.write("Hello World!\n"); // Write a response to the client
+        response.end("This is the end"); // End response
+    })
+    .listen(PORT); // The server object listens on port
