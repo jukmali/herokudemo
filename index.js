229462d590baa48c497e18b5dd85eff0d388e685
jukmali <54353202+jukmali@users.noreply.github.com>
2022-01-26 20:50:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var http = require("http");
 http
     .createServer(function(request,response){
         response.writeHead(200,{"Content-Type":"text/plain"});
-        response.write("Hello World!\n"); // Write a response to the client
-        response.end("This is the end"); // End response
+        response.write("This is the first cloud demonstration!\n"); // Write a response to the client
+        response.end("--Jukka--"); // End response
     })
     .listen(PORT); // The server object listens on port
